37c8936b2442e1e16c3c2d678c230c7668578a0f
konstantinos193 <96600944+konstantinos193@users.noreply.github.com>
2025-01-08 15:38:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -442,7 +442,7 @@ async function assignDiscordRoles(sessionId, walletAddress, hasStakedNFTs) {
   }
 }
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
 
 app.get('/api/discord/:sessionId/wallets', (req, res) => {
   try {
