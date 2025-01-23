5412b944635b5a11b6d4cf6e8145c9fcfe8f7d6c
konstantinos193 <96600944+konstantinos193@users.noreply.github.com>
2025-01-23 03:47:45 +0200

    Add files via upload

---
+++ trackedCollections.js
@@ -0,0 +1,31 @@
+// List of collections to track floor prices
+const TRACKED_COLLECTIONS = [
+  {
+    id: 1,
+    name: "Ape Elite Club",
+    contractAddress: "0x485242262f1e367144fe432ba858f9ef6f491334",
+    magicEdenSymbol: "apeeliteclub" // Magic Eden collection symbol/slug
+  },
+  {
+    id: 2,
+    name: "Gobs On Ape",
+    contractAddress: "0xbebaa24108d6a03c7331464270b95278bbbe6ff7",
+    magicEdenSymbol: "gobsonape"
+  },
+  {
+    id: 3,
+    name: "Gs on Ape",
+    contractAddress: "0xb3443b6bd585ba4118cae2bedb61c7ec4a8281df",
+    magicEdenSymbol: "gsonape"
+  },
+  {
+    id: 4,
+    name: "TokenGators",
+    contractAddress: "0xd33edec311f8769c71f132a77f0c0796c22af1c5",
+    magicEdenSymbol: "tokengators"
+  }
+];
+
+module.exports = {
+  TRACKED_COLLECTIONS
+}; 
